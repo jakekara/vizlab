630323a0194254ba87e93c779975b89b37769710
Jake Kara <jake@jakekara.com>
2019-03-08 13:44:30 -0500

    Stuff

---
+++ src/ConnTownMap/drawMap.js
@@ -0,0 +1,46 @@
+var connTowns = require("./shapes/ct-towns.json")
+var topojson = require("topojson");
+// var d3 = require("d3");
+
+var d3 = Object.assign({},
+    require("d3-selection"),
+    require("d3-geo"),
+);
+
+export default function(config){
+
+    var root = d3.select(config.root).html(""),
+    bbox = root.node().getBoundingClientRect(),
+    width = bbox.width,
+    height = width / 1.65,
+    outlineFilter = config.outlineFilter || function(){ return true };
+
+    var svg = d3.select("#root").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+    
+    var projection = d3.geoMercator()
+    .rotate([72.7, 0, 0])
+    .center([0, 41.52])
+    .scale(width * 22)
+    .translate([width/ 2, height / 2]);;
+
+    var path = d3.geoPath()
+    .projection(projection)    
+
+    var towns = svg.selectAll('.town')
+        .data(topojson.feature(connTowns, connTowns.objects.towns).features)
+        .enter()
+        .append('path')
+        .classed('town', true)
+        .attr('d', path)
+        .attr("data-town", function(d){ return d.id})
+
+    // draw the outline of the state
+    var state = svg.append("path")
+        .style("stroke-width", 2)
+        .classed("state", true)
+        .attr("d", path(topojson.merge(connTowns, connTowns.objects.towns.geometries.filter(outlineFilter))));
+
+    
+}
--- src/ConnTownMap/index.js
+++ src/ConnTownMap/index.js
@@ -1,18 +1,7 @@
-
+"use strict";
 import ThrottledViz  from "../ThrottledViz";
 import "./style/main.scss";
-
-var connTowns = require("./shapes/ct_towns.json");
-var topojson = require("topojson");
-
-var d3 = Object.assign({},
-    require("d3-selection"),
-    require("d3-geo"),
-);
-
-// var d3 = Object.assign({},
-//     require("d3-selection"),
-//     require("d3-geo"));
+import drawMap from "./drawMap";
 
 class ConnTownMap extends ThrottledViz {
 
@@ -23,33 +12,45 @@ class ConnTownMap extends ThrottledViz {
     }
 
     draw(){
-        var root = d3.select("#root").html(""),
-        bbox = root.node().getBoundingClientRect(),
-        width = bbox.width,
-        height = width / 1.65;
-    
-        var svg = d3.select("#root").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-        
-        var projection = d3.geoMercator()
-        .rotate([72.7, 0, 0])
-        .center([0, 41.52])
-        .scale(width * 22)
-        .translate([width/ 2, height / 2]);;
-    
-        var path = d3.geoPath()
-        .projection(projection)    
-    
-        this.towns = svg.selectAll('.town')
-            .data(topojson.feature(connTowns, connTowns.objects.ct_towns_s).features)
-            .enter()
-            .append('path')
-            .attr('class', 'town')
-            .attr('d', path)
-            .attr("data-town", function(d){ return d.id})
-    
+        var config = this.config;
+        config.root = "#root"
+        drawMap(this.config)
     }
+
+    // draw(){
+    //     var root = d3.select("#root").html(""),
+    //     bbox = root.node().getBoundingClientRect(),
+    //     width = bbox.width,
+    //     height = width / 1.65,
+    //     outlineFilter = this.config.outlineFilter || function(){ return true };
+    
+    //     var svg = d3.select("#root").append("svg")
+    //     .attr("width", width)
+    //     .attr("height", height);
+        
+    //     var projection = d3.geoMercator()
+    //     .rotate([72.7, 0, 0])
+    //     .center([0, 41.52])
+    //     .scale(width * 22)
+    //     .translate([width/ 2, height / 2]);;
+    
+    //     var path = d3.geoPath()
+    //     .projection(projection)    
+    
+    //     this.towns = svg.selectAll('.town')
+    //         .data(topojson.feature(connTowns, connTowns.objects["cb_2017_09_cousub_500k"]).features)
+    //         .enter()
+    //         .append('path')
+    //         .classed('town', true)
+    //         .attr('d', path)
+    //         .attr("data-town", function(d){ return d.id})
+
+    //     // draw the outline of the state
+    //     var state = svg.append("path")
+    //         .style("stroke-width", 2)
+    //         .classed("state", true)
+    //         .attr("d", path(topojson.merge(connTowns, connTowns.objects.cb_2017_09_cousub_500k.geometries.filter(outlineFilter))));
+    // }
 }
 
 export {ConnTownMap};
